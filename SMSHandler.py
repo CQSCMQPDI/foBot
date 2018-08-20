e2976f9e8a6f9e08c6677265578ab3f316f5f75b
louis chauvet <louis.chauvet@free.fr>
2018-08-20 18:27:52 +0200

    Repair load_module, and smshandler

--- SMSHandler.py
+++ SMSHandler.py
@@ -7,6 +7,9 @@ import os
 class SMSHandler(logging.Handler):
     def emit(self, record):
         msg = urllib.parse.quote(self.format(record).encode('utf8'))
-        urllib.request.urlopen("https://smsapi.free-mobile.fr/sendmsg?user=" +
+        try:
+            urllib.request.urlopen("https://smsapi.free-mobile.fr/sendmsg?user=" +
                                os.environ.get("FREE_SMS_USER", "") + "&pass=" +
                                os.environ.get("FREE_SMS_PASS", "") + "&msg=" + msg)
+        except urllib.error.HTTPError:
+            pass
